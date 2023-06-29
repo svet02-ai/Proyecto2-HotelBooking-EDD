affd8d353796cf0439d540a01fe910ae1d2e1555
santiagoB0 <san8tiago00@gmail.com>
2023-06-29 18:18:36 -0400

    Interface gráfica

--- hotel_booking/src/hotel_booking/Main.java
+++ hotel_booking/src/hotel_booking/Main.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package hotel_booking;
-
+import Interfaces.WelcomeWindow;
 /**
  *
  * @author Svetlana Valentina
@@ -14,7 +14,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        WelcomeWindow w1 = new WelcomeWindow();
+        w1.setVisible(true);
     }
     
 }
