d0a72a856a7ddd8cb0e7e5a62d4872da9355b7f8
santiagoB0 <san8tiago00@gmail.com>
2023-06-29 22:39:02 -0400

    arreglo de peueño error

--- hotel_booking/src/hotel_booking/Main.java
+++ hotel_booking/src/hotel_booking/Main.java
@@ -16,12 +16,10 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         System.out.println("Hola");
-        // TODO code application logic here
-=======
+
         App.Start();
->>>>>>> 3df6f1069ced98559569da5100f0fd1dce3dd433
     }
     
 }
